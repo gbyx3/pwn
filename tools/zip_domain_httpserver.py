5f34d2f55a2e860907cc6530e1587c0ed3ddecf3
Gustav <g.alerby@gmail.com>
2023-11-08 22:12:16 +0100

    Go home copilot your drunk...

--- tools/zip_domain_httpserver.py
+++ tools/zip_domain_httpserver.py
@@ -1,26 +1,26 @@
 # This thing serves a web page that reads and parses the basic auth headers
 
-from bottle import auth_basic, request, route, run, response
+from bottle import auth_basic, request, route, run, response, hook
 import base64
 
 port = 8080
 debug = True
 
+
 def read_basic_auth(username, password):
     auth = request.get_header('Authorization')
     if auth:
-        (basic, encoded) = auth.split(' ')
+        basic, encoded = auth.split(' ')
         decoded = base64.b64decode(encoded).decode("utf-8")
-        (username, password) = decoded.split(':')
-        print("Username: %s, Password: %s" % (username, password))
-        return True
-    print("No auth header received.")
-    return False
+        username, password = decoded.split(':')
+        print(f"Username: {username}\nPassword: {password}")
+    return True
 
 
 @route('/')
 @auth_basic(read_basic_auth)
 def index():
+    print(json.dumps(dict(request.headers), indent=4, sort_keys=True))
     response.status = 200
     return "Hello, world!"
 
